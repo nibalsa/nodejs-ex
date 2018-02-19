49a1da313893c8842c75064d626921227e24ab41
nedSaf <nader@gizra.com>
2018-02-19 10:23:59 +0200

    Restore page count

--- server.js
+++ server.js
@@ -82,8 +82,19 @@ app.get('/', function (req, res) {
   if (!db) {
     initDb(function(err){});
   }
-
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
 });
 
 //INDEX - displays all the contacts that we have in the DB
@@ -171,6 +182,21 @@ app.delete("/contacts/:id", function(req, res){
       }
    });
 
+});
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
 });
 
 
